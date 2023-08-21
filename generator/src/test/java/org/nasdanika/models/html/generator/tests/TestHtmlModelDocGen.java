5fa442a0ad62faba739e0b6f5eca9b11e07f09d9
Pavel <Pavel.Vlasov@nasdanika.org>
2023-08-21 11:52:36 -0400

    Sitemap fix

--- generator/src/test/java/org/nasdanika/models/html/generator/tests/TestHtmlModelDocGen.java
+++ generator/src/test/java/org/nasdanika/models/html/generator/tests/TestHtmlModelDocGen.java
@@ -172,7 +172,7 @@ public class TestHtmlModelDocGen {
 		String pageTemplateResource = "page-template.yml";
 		URI pageTemplateURI = URI.createFileURI(new File(pageTemplateResource).getAbsolutePath());//.appendFragment("/");
 		
-		String siteMapDomain = "https://gitlab.models.nasdanika.org";		
+		String siteMapDomain = "https://html.models.nasdanika.org";		
 		ActionSiteGenerator actionSiteGenerator = new ActionSiteGenerator() {
 			
 			protected boolean isDeleteOutputPath(String path) {
